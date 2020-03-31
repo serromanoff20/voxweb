adb662431665e5d25cee3b813025dbcd871baf42
Sergey Romanov <serromanoff2015@yandex.ru>
2020-03-31 15:22:02 +0400

    simple_verstka

---
+++ VoxWeb/script.js
@@ -0,0 +1,10 @@
+let dropMenu = document.getElementById('DropMenu');
+
+function DropOnLeft(){	
+	dropMenu.style.display='block';
+};
+
+function closeMenu()
+{
+	dropMenu.style.display='none';	
+};
